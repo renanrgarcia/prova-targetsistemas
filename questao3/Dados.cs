7ad37a0409be8cfccc6e71e7f6e89678afab2d6c
Renan Garcia <renanrgarcia@gmail.com>
2025-01-10 15:06:39 -0300

    Questão 3

---
+++ questao3/Dados.cs
@@ -0,0 +1,8 @@
+namespace questao3
+{
+    public class Dados
+    {
+        public int Dia { get; set; }
+        public double Valor { get; set; }
+    }
+}
---
+++ questao3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Newtonsoft.Json;
+using questao3;
+
+string filePath = Path.Combine(Directory.GetCurrentDirectory(), "dados.json");
+
+string json = File.ReadAllText(filePath);
+
+List<Dados>? dados = JsonConvert.DeserializeObject<List<Dados>>(json);
+
+double valorMaximo = dados.Max(d => d.Valor);
+double valorMinimo = dados.Min(d => d.Valor);
+double media = dados.Average(d => d.Valor);
+int diasAcimaDaMedia = dados.Count(d => d.Valor > media);
+
+Console.WriteLine($"Valor máximo: {valorMaximo:F2}");
+Console.WriteLine($"Valor mínimo: {valorMinimo}");
+Console.WriteLine($"Média: {media:F2}");
+Console.WriteLine($"Dias acima da média: {diasAcimaDaMedia}");
