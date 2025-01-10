34c48b1133eeba6b73a4f20c3c1380ae61f45044
Renan Garcia <renanrgarcia@gmail.com>
2025-01-10 15:49:52 -0300

    Questão 5

---
+++ questao5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite o texto que deseja inverter: ");
+
+string texto = Console.ReadLine();
+
+List<char> novoTexto = new List<char>();
+
+for (int i = texto.Length - 1; i >= 0; i--)
+{
+    novoTexto.Add(texto[i]);
+}
+
+Console.WriteLine([.. novoTexto]);
