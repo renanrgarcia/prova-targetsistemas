81c1b30b04ec33161046e04c42ffba917d017f90
Renan Garcia <renanrgarcia@gmail.com>
2025-01-10 15:15:51 -0300

    Questão 4

---
+++ questao4/Estado.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace questao4
+{
+    public class Estado
+    {
+        public string Nome { get; set; }
+        public double Faturamento { get; set; }
+    }
+}
---
+++ questao4/Program.cs
@@ -0,0 +1,17 @@
+﻿using questao4;
+
+List<Estado> estados = new List<Estado>
+    {
+        new Estado { Nome = "SP", Faturamento = 67836.43 },
+        new Estado { Nome = "RJ", Faturamento = 36678.66 },
+        new Estado { Nome = "MG", Faturamento = 29229.88 },
+        new Estado { Nome = "ES", Faturamento = 27165.48 },
+        new Estado { Nome = "Outros", Faturamento = 19849.53 }
+    };
+
+double total = estados.Sum(e => e.Faturamento);
+
+foreach (var estado in estados)
+{
+    Console.WriteLine($"{estado.Nome} - {estado.Faturamento / total:P}");
+}
